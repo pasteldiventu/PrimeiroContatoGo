35f6d46caaa2424224e3d9d38c64ccb2ace59554
João Pedro Oliveira <122324367+pasteldiventu@users.noreply.github.com>
2024-04-07 16:39:05 -0400

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,69 +1,112 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
+const monitoramentos = 2
+const delay = 5
+
 func main() {
-
 	exibeIntroducao()
-	for {
 
+	for {
 		exibeMenu()
 
 		comando := leComando()
 
 		switch comando {
-
 		case 1:
 			iniciarMonitoramento()
-
-		case 2:
-			fmt.Println("Exibindo logs...")
-
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
-
 		default:
-			fmt.Println("Comando não reconhecido")
+			fmt.Println("Não conheço este comando")
 			os.Exit(-1)
 		}
 	}
+
 }
 
 func exibeIntroducao() {
-	nome := "vascao" //definição da variavel nao é obrigatoria, nome := "vascao" funciona igual var nome string = "vascao"
-	var versao float32 = 1.2
-
-	fmt.Println("fala ", nome)
-	fmt.Println("essa é a versão", versao, "do programa")
+	nome := "Douglas"
+	versao := 1.2
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
 }
 
 func exibeMenu() {
-	fmt.Println("1- iniciar monitoramento")
-	fmt.Println("2- exibir logs")
-	fmt.Println("0- Sair")
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("0- Sair do Programa")
 }
 
 func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
-	fmt.Println("O comando escolhido foi:", comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://univagead.com.br/avaunivag"
-	resp, _ := http.Get(site)
+	sites := leSitesDoArquivo()
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
 
 	if resp.StatusCode == 200 {
-		fmt.Println("Site: ", site, " está funcionando corretamente!")
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
-		fmt.Println("o Site: ", site, "deu erro no carregamento. Status code:", resp.StatusCode)
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
 	}
+}
+
+func leSitesDoArquivo() []string {
+	var sites []string
+	arquivo, err := os.Open("sites.md")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+
+	}
+
+	arquivo.Close()
+	return sites
 }
